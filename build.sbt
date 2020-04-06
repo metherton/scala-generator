032283c5601db8b348a30e0edb8cddcc3c3bf500
metherton <martinetherton63@gmail.com>
2020-04-06 21:03:47 +0200

    Added map

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "generator"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
--- src/main/scala/Generator.scala
+++ src/main/scala/Generator.scala
@@ -1,5 +1,13 @@
 package com.martinetherton
 
 trait Generator[+T] {
+
+  self =>
+
   def generate: T
+
+  def map[S](f : T => S): Generator[S] = new Generator[S] {
+    override def generate: S = f(self.generate)
+  }
+
 }
